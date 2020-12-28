aa86bb5b654a0273598f930b0400b6f296942855
hoangthinh9713 <hoangvanthinh9713@gmail.com>
2020-12-28 13:55:28 +0900

    queue 完了

---
+++ _queue/main.py
@@ -0,0 +1,26 @@
+from collections import deque
+from typing import Any
+
+class Queue(object):
+    
+    def __init__(self) -> None:
+        self.queue = []
+
+    def enqueue(self, data: Any) -> None:
+        self. queue.append(data)
+
+    def dequeue(self) -> Any:
+        if self.queue:
+            return self.queue.pop(0)
+
+
+if __name__=='__main__':
+    q = deque()
+    q.append(1)
+    q.append(2)
+    q.append(3)
+    q.append(4)
+    print(q)
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
